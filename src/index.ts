9c18a382165478857024b5cb340e138a971fbecc
babas5 <babasak54@gmail.com>
2026-03-02 15:03:28 +0100

    Fix: added file extension to app import

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { app } from './app.js'
 
-const app = new Hono()
-
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+const port = 3000
 
 serve({
   fetch: app.fetch,
   port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
 })
+
+console.log('Port 3000')
+
+
